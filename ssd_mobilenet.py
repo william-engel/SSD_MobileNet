d26396a33bed4114e3526efa186fdf382e4b3474
William Engel <william.engel@web.de>
2021-02-01 21:41:59 +0100

    add regularization

--- ssd_mobilenet.py
+++ ssd_mobilenet.py
@@ -1,12 +1,13 @@
 from tensorflow.keras.layers import Conv2D, BatchNormalization, ReLU, DepthwiseConv2D, Input, GlobalAveragePooling2D, Reshape, Dropout, Activation, Concatenate
+from tensorflow.keras.regularizers import l2
 from tensorflow.keras import Model
 
 from mobilenetv1 import MobileNet
 
-def SSD_MobileNet(num_classes,feature_layers):
+def SSD_MobileNet(num_classes, box_specs_list):
 
   def _conv_block(input, filters, kernel_size = (3, 3), strides = (1, 1), block_id = 0):
-    x = Conv2D(filters, kernel_size, strides, padding = 'same', use_bias = False, name = 'conv%s' % block_id)(input)
+    x = Conv2D(filters, kernel_size, strides, padding = 'same', use_bias = False, kernel_initializer = 'he_normal', kernel_regularizer = l2(1e-4), name = 'conv%s' % block_id)(input)
     x = BatchNormalization(name = 'conv%s_bn' % block_id)(x)
     x = ReLU(6., name = 'conv%s_relu' % block_id)(x) # ReLU6
 
@@ -37,20 +38,21 @@ def SSD_MobileNet(num_classes,feature_layers):
   x = _conv_block(input = x, filters = 128, kernel_size = (3,3), strides = (2,2), block_id = '17_2') # 1x1
   outputs.append(x)
 
-  # add boxpredictor
+  # add bbox predictor
   loc_total, cls_total = [], []
 
   for index, output in enumerate(outputs):
-    assert output.shape[1:3] == feature_layers[index]['shape'], 'Shapes do not match. Model output_shape is %s, expected shape is %s.' % (output.shape[1:3], feature_layers[index]['shape'])
-    num_df = len(feature_layers[index]['aspect_ratios']) + 1
+    num_df = len(box_specs_list[index]) # number default boxes
 
     # class
-    cls = Conv2D(filters = num_df * num_classes, kernel_size = (1,1), padding = 'same', kernel_initializer = 'glorot_normal', name = 'cls_predictor_%s' %index)(output)
+    cls = Conv2D(filters = num_df * num_classes, kernel_size = (1,1), padding = 'same', 
+                 kernel_initializer = 'he_normal', kernel_regularizer = l2(1e-4), name = 'cls_predictor_%s' %index)(output)
     cls = Reshape([-1, num_classes], name = 'reshape_cls_%s' %index)(cls)
     cls_total.append(cls)
 
     # localisation
-    loc = Conv2D(filters = num_df * 4, kernel_size = (1,1), padding = 'same', kernel_initializer = 'glorot_normal', name = 'loc_predictor_%s' %index)(output)
+    loc = Conv2D(filters = num_df * 4, kernel_size = (1,1), padding = 'same',
+                 kernel_initializer = 'he_normal', kernel_regularizer = l2(1e-4), name = 'loc_predictor_%s' %index)(output)
     loc = Reshape([-1, 4], name = 'reshape_loc_%s' %index)(loc)
     loc_total.append(loc)
 
